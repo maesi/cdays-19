e5b196e37e49878e2abcba3438736324e63d6397
maesi <mail@maesi.org>
2019-05-01 00:52:13 +0200

    remove firebase test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
-import { User } from './types/user';
 
 @Component({
   selector: 'app-root',
@@ -9,14 +6,4 @@ import { User } from './types/user';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  itemsCollection: AngularFirestoreCollection<User>;
-  items: Observable<User[]>;
-
-  constructor(private afs: AngularFirestore) {
-    this.itemsCollection = afs.collection<User>('user');
-    this.items = this.itemsCollection.valueChanges();
-  }
-
-
 }
