04f658791898bc844de95f753347a70080b2e2bd
maesi <mail@maesi.org>
2019-04-30 23:26:56 +0200

    null anstelle von undefined für username verwenden

--- src/app/session.service.ts
+++ src/app/session.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 
+const localStorageKey = "cdays-username";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,24 +11,24 @@ export class SessionService {
   private username: string;
 
   constructor() { 
-    this.username = localStorage.getItem('username');
+    this.username = localStorage.getItem(localStorageKey);
   }
 
   public isLoggedIn(): boolean {
-    return this.username !== undefined;
+    return this.username !== null;
   }
 
   public login(username: string, password: string): Observable<boolean> {
     let ok = username === password;
     if (ok) {
       this.username = username;
-      localStorage.setItem('username', this.username);
+      localStorage.setItem(localStorageKey, this.username);
     }
     return of(ok);
   }
 
   public logout(): void {
-    localStorage.removeItem('username');
-    this.username = undefined;
+    localStorage.removeItem(localStorageKey);
+    this.username = null;
   }
 }
