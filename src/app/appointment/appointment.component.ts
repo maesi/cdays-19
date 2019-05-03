63f9b3c2ca2fc0d8be87ff0b0bd3e12effcdf069
(U116399) <marcel.arnold@mobi.ch>
2019-05-03 07:58:31 +0200

    Event-Formular implementiert

--- src/app/appointment/appointment.component.ts
+++ src/app/appointment/appointment.component.ts
@@ -4,6 +4,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 import {Topic} from '../types/topic';
 import {NotifikationTyp} from '../types/notifikation';
 import {Meeting} from '../types/meeting';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-appointment',
@@ -11,42 +12,49 @@ import {Meeting} from '../types/meeting';
   styleUrls: ['./appointment.component.scss']
 })
 export class AppointmentComponent implements OnInit {
-
+  form: FormGroup | any;
   topic: Topic;
   meeting: Meeting;
   createMode: boolean;
 
-  constructor(private storageService: StorageService, private route: ActivatedRoute, private router: Router) {
+  constructor(private storageService: StorageService, private route: ActivatedRoute, private router: Router,
+              private formBuilder: FormBuilder) {
     this.createMode = this.router.url.endsWith('create');
   }
 
   ngOnInit() {
     this.route.paramMap.subscribe(params => {
       const id = parseInt(params.get('id'));
-      if(this.createMode) {
+      if (this.createMode) {
         this.topic = this.storageService.getTopic(id);
       } else {
         this.meeting = this.storageService.getMeeting(id);
         this.topic = this.meeting.topic;
       }
     });
+    if(this.createMode) {
+      this.form = this.formBuilder.group({
+        ort: ['Bern', Validators.required],
+        datum: ['2019-05-14', Validators.required]
+      });
+    }
   }
 
-  create() {
+  onCreate() {
     const meeting = <Meeting>{
       id: this.storageService.getMeetingId(),
-      ort: 'Nyon',
-      datum: '2019-05-05',
+      ort: this.form.controls.ort.value,
+      datum: this.form.controls.datum.value,
       participate: true,
       created: true,
       topic: this.topic
     };
     this.storageService.addMeeting(meeting);
-    if(meeting.id === 1) {
+    if (meeting.id === 1) {
       setTimeout(() => {
         const meeting = <Meeting>{
           id: this.storageService.getMeetingId(),
-          ort: 'Bern',
+          ort: 'Nyon',
           datum: '2019-05-12',
           participate: false,
           created: false,
